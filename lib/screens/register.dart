156b5ed36ee5ea1a2a6373f3fa57fb6af6e3320d
PersianBlue <71480723+PersianBlue@users.noreply.github.com>
2022-07-07 11:59:54 -0500

    added decoration for text input fields

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -2,6 +2,7 @@
 
 import "package:flutter/material.dart";
 import 'package:flutter_coffee_brew/services/auth.dart';
+import 'package:flutter_coffee_brew/shared/constants.dart';
 
 class Register extends StatefulWidget {
   final Function toggleView;
@@ -47,6 +48,7 @@ class _RegisterState extends State<Register> {
               children: [
                 SizedBox(height: 20.0),
                 TextFormField(
+                  decoration: textInputDecoration.copyWith(hintText: "Email"),
                   validator: (val) => val!.isEmpty ? "Enter an email" : null,
                   onChanged: (value) {
                     setState(() => email = value);
@@ -54,6 +56,8 @@ class _RegisterState extends State<Register> {
                 ),
                 SizedBox(height: 20.0),
                 TextFormField(
+                  decoration:
+                      textInputDecoration.copyWith(hintText: "Password"),
                   validator: (val) =>
                       val!.length < 6 ? "Enter a password 6+ chars long" : null,
                   onChanged: (value) {
